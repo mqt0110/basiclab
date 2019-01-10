802673d7a2488a0cc3273ec4b6795d3ef1dc3377
Quoc Tuan Mai <33127923+mqt0110@users.noreply.github.com>
2019-01-10 20:49:19 +0100

    Add files via upload

---
+++ task5a.cpp
@@ -0,0 +1,604 @@
+#include <iostream>
+extern "C" {
+#include <string.h>
+}
+
+using namespace std;
+
+class Photo{
+
+        int code;
+        int dpi;
+        char *keyword1;
+        char *keyword2;
+        char *keyword3;
+        char *name;
+        Photo *next;
+ public :
+         Photo(int code,int dpi,char *keyword1,char *keyword2,char *keyword3,char *name) : code(code), dpi(dpi)
+         {
+             int len=strlen(name);
+             this->name= new char [len+1];
+             strcpy(this->name, name);
+
+            len=strlen(keyword1);
+            this->keyword1= new char [len+1];
+            strcpy(this->keyword1, keyword1);
+
+            len=strlen(keyword2);
+            this->keyword2= new char [len+1];
+            strcpy(this->keyword2, keyword2);
+
+            len=strlen(keyword3);
+            this->keyword3= new char [len+1];
+            strcpy(this->keyword3, keyword3);
+
+            this->next = NULL;
+         }
+
+         ~Photo()
+         {
+
+             delete  name;
+             delete  keyword1;
+             delete  keyword2;;
+             delete  keyword3;
+
+         }
+    friend class Lensman;
+}; //dau ket thuc class
+class Lensman{
+    public:
+        Photo *head;
+     int add_Photo(int dpi, char *keyword1, char *keyword2, char *keyword3, char *name)
+    {
+        Photo *current = head;
+        Photo *tmp= NULL;
+        Photo *previous=head;
+
+
+        int a,b,c,d,e,b1,b2,b3,c1,c2,c3,d1,d2,d3,len;
+        int mark=0;
+        int maxcode=0;
+        int delcode;
+        // lenh check phan dau, neu trong thi them anh moi
+        if(!head)  // neu head trong, chi them anh
+            {
+            head= new Photo(1,dpi,keyword1,keyword2,keyword3,name);
+            maxcode++;
+            return 0;
+            }
+        else{ //head khong trong, so sanh
+        //head  khong trong thi bat dau check
+        while (current) //kiem tra con tro hien tai
+        {
+
+
+            //CHECK NAME VA KEYWORD
+            a=strcmp(current->name, name);
+
+            b1=strcmp(current->keyword1,keyword1);
+            b2=strcmp(current->keyword1,keyword2);
+            b3=strcmp(current->keyword1,keyword3);
+
+            c1=strcmp(current->keyword2,keyword1);
+            c2=strcmp(current->keyword2,keyword2);
+            c3=strcmp(current->keyword2,keyword3);
+
+            d1=strcmp(current->keyword3,keyword1);
+            d2=strcmp(current->keyword3,keyword2);
+            d3=strcmp(current->keyword3,keyword3);
+
+            e=current->dpi - dpi;
+
+        if((b1 == 0) || (b2 == 0) || (b3 == 0))
+            b=0;
+        else
+            b=-1;
+
+
+        if((c1 == 0) || (c2 == 0) || (c3 == 0))
+            c=0;
+        else
+            c=-1;
+
+
+        if((d1 == 0) || (d2 == 0) || (d3 == 0))
+            d=0;
+        else
+            d=-1;
+
+            //check dieu kien
+            //1: cung ten va key
+            if( (a == 0) && ( b==0)&& (c==0) && (d==0) )
+                {
+                    if(e>=0) //dpi cu lon hon dpi moi
+                        {
+                            mark=-1;
+                            return -1;
+                            break;// bo qua va ngung  duyet list  --- viec xe t nay chi co 1 , neu trung vs 1 phan tu thi co the break, bo qua vong lap while(current)  /// khong thoat vong lap do can di den cuoi list, chi xoa con tro hien tai
+                        }
+                    else // thay dpi, code va doi vi tri --- cũng chi co 1 phan tu va co the break sau khi xong viec
+                    { //co 3 truong hop, 1: head , 2: mot vi tri bat ky trong lít, 3: cuoi list
+                        mark=-2;
+                        delcode=current->code;
+                         // ghi du lieu cho con tro tam int len=strlen(name);
+
+                        tmp=new Photo(current->code, dpi, current->keyword1,current->keyword2,current->keyword3,current->name);  // ghi du lieu cho con tro tam
+                    // xoa con tro current
+                        if(current == head)
+                            head=head->next;
+                        else
+                        {
+                            previous->next=current->next;
+                        }
+                        current=current->next; //tiep tuc duyet list, khong break do can tao file o cuoi
+
+                    }
+                }
+
+            //2: khac ten
+           else
+               {
+
+              if(current->next)
+                  {
+                        previous=current;
+                        current=current->next;  //tiep tuc duyet list
+
+                  }
+                else
+               {  //cuoi list
+                    if(mark == 0)
+                        {
+                            current->next=new Photo(current->code+1, dpi, keyword1,keyword2,keyword3,name);
+                        return 0;
+                        }
+                    else if(mark == -2)
+                        {
+
+                        current->next=tmp;
+                        current->next->code = current->code+1;
+
+                        return 0;
+                        }
+               }
+              }
+            } // dau ket thuc lenh while
+
+
+        }//ket thuc lenh else
+
+
+}//dau ket thuc function
+
+    void print()
+    {
+        Photo *current=head;
+            cout<<"Photo list :" << endl;
+            while (current)
+            {
+                cout<<"Code: " << current->code<<endl;
+                cout<<"Name: " << current->name<<endl;
+                cout<<"Dpi:  " << current->dpi<<endl;
+                cout<<"Keyword 1 : "<<current->keyword1<<endl;
+                cout<<"Keyword 2 : "<<current->keyword2<<endl;
+                cout<<"Keyword 3 : "<<current->keyword3<<endl;
+                cout<<endl;
+
+            current=current->next;
+            }
+            cout<<endl;
+            cout<<endl;
+            cout<<"End of list"<<endl;
+            cout<<endl;
+    }
+
+    void remove_Photo(int code)
+    {
+        Photo *current=head;
+        Photo *previous= NULL;
+        if( head == NULL)
+            return;
+
+        while( (current->code)!= code)
+            {
+                if(!(current->next))
+                    return;
+                else
+                {
+                previous=current;  //khai bao 1 con tro du phong de xoa con tro hien tai
+                current=current->next;
+                }
+            }
+
+        if ( current == head)
+            {
+                head= head->next;  //xoa bo con tro head, thay head moi
+            }
+        else
+            {
+                previous->next = current -> next; //bo con tro current
+            }
+        return; //tra ve danh sach
+    }
+
+    int &modify(int dpi, char* keyword1, char* keyword2, char*keyword3, char* name)
+{
+
+    Photo *current=head;
+
+
+    int a,b1,b2,b3,c1,c2,c3,d1,d2,d3,len1,len2;
+
+    //khong xet head, yeu cau tao head truoc
+        if(!head)
+        {
+            cout<<"Cant modify";
+        }
+        else{
+        while(current)// xet con tro current
+        {
+        //thiet lap gia tri a,b1,b2,b3,c1,c2,c3,d1,d2,d3
+        a=strcmp(current->name,name);
+
+        b1=strcmp(current->keyword1,keyword1);
+        b2=strcmp(current->keyword1,keyword2);
+        b3=strcmp(current->keyword1,keyword3);
+
+        c1=strcmp(current->keyword2,keyword1);
+        c2=strcmp(current->keyword2,keyword2);
+        c3=strcmp(current->keyword2,keyword3);
+
+        d1=strcmp(current->keyword3,keyword1);
+        d2=strcmp(current->keyword3,keyword2);
+        d3=strcmp(current->keyword3,keyword3);
+
+        //xet dieu kien
+
+        if (((b1 == 0) || (b2 == 0) || (b3 == 0) ) && ( (c1 == 0) || (c2 == 0) || (c3 == 0) ) &&( (d1 == 0) || (d2 == 0) || (d3 == 0) ) )
+        {
+            if( a == 0)
+            {// cung ten va key , chi co 1 truong hop nen khi phat hien lap tuc break
+            current->dpi=dpi;
+            return current->dpi;
+            break;
+            }
+            else if (a!=0) // thay ten ,khi cung key nhung khac ten, co the co 2 truong hop
+            {
+            current->dpi=dpi; // thay dpi
+            len1=strlen(name);
+            len2=strlen(current->name);
+
+            if(len1 >= len2)
+                {
+                strncpy(current->name,name,len1);
+                current->name[len1]='\0';
+                }
+            else
+                strncpy(current->name,name,len2);
+
+
+            return current->dpi;
+            break;
+            }
+        } // ngat lenh if
+        else
+        {
+
+            // kiem tra dieu kien de tao moi neu khong xay ra 2 truong hop tren
+           if (current->next)
+               {
+                   current=current->next; //tiep tuc duyet list
+               }
+            else //phan tu cuoi list
+                {
+                    current->next=new Photo((current->code+1),dpi,keyword1,keyword2,keyword3,name);
+                   return current->next->dpi;
+                    break;
+               }
+
+       }// ngat lenh else
+        }//ngat lenh while(current)
+        }//ngat lenh else
+
+
+
+
+
+}//dau ket thuc function
+    Lensman& operator = (const Lensman & x) // assignment operator- passed in pointer
+    {
+
+        Photo *oldlist=x.head; // con tro list goc
+
+        if (this == &x)
+            return *this;
+
+
+            // lam sach con tro this
+           this->~Lensman(); // clear *this
+
+            //copy tu optr den cptr
+            while(oldlist)
+            {
+                this->add_Photo(oldlist->dpi,oldlist->keyword1,oldlist->keyword2,oldlist->keyword3,oldlist->name);
+                oldlist=oldlist->next;
+            }
+
+
+        cout<<"Assignment operator Finished"<<endl;
+        return *this;
+
+    }
+
+
+   Lensman (const Lensman& x)  //copy constructor  // passed in pointer
+        {
+
+            Photo *oldlist=x.head; // con tro list goc
+            this->head=NULL;
+            while(oldlist) // duyet con tro goc tu phan tu thu hai
+            {
+
+                       this->add_Photo(oldlist->dpi,oldlist->keyword1,oldlist->keyword2,oldlist->keyword3,oldlist->name);
+                oldlist=oldlist->next;
+
+            }
+
+           cout<<"Copy constructor finished"<<endl;  // test
+}
+
+    Lensman operator +(const Lensman &x) // c=a+b ,  b=x  => a:list 1(con tro this), b:list2  -- copy list2 vao list 1    /// passed
+    {
+        Lensman *newlist=new Lensman(*this);
+        Photo *currentlist1=this->head;
+        Photo *currentlist2=x.head;
+
+        int a,b,c,d,b1,b2,b3,c1,c2,c3,d1,d2,d3;
+        int mark;
+
+        if(!(currentlist1)) // list 1 trong
+        {
+            *newlist=x;
+        }
+        if ((currentlist1) && (currentlist2) )
+            {
+
+
+                // xet list 2
+                while(currentlist2)
+                {
+                    mark=0;
+                    while(currentlist1)
+                    {
+
+                         //CHECK NAME VA KEYWORD
+            a=strcmp(currentlist1->name,currentlist2->name);
+
+            b1=strcmp(currentlist1->keyword1,currentlist2->keyword1);
+            b2=strcmp(currentlist1->keyword1,currentlist2->keyword2);
+            b3=strcmp(currentlist1->keyword1,currentlist2->keyword3);
+
+            c1=strcmp(currentlist1->keyword2,currentlist2->keyword1);
+            c2=strcmp(currentlist1->keyword2,currentlist2->keyword2);
+            c3=strcmp(currentlist1->keyword2,currentlist2->keyword3);
+
+            d1=strcmp(currentlist1->keyword3,currentlist2->keyword1);
+            d2=strcmp(currentlist1->keyword3,currentlist2->keyword2);
+            d3=strcmp(currentlist1->keyword3,currentlist2->keyword3);
+
+        if((b1 == 0) || (b2 == 0) || (b3 == 0))
+            b=0;
+        else
+            b=-1;
+
+
+        if((c1 == 0) || (c2 == 0) || (c3 == 0))
+            c=0;
+        else
+            c=-1;
+
+
+        if((d1 == 0) || (d2 == 0) || (d3 == 0))
+            d=0;
+        else
+            d=-1;
+        ////xet dieu kien
+        //1:cung key va ten
+
+                if( (a == 0) && ( b==0)&& (c==0) && (d==0) )
+                {
+                    mark=-1;
+                    break; //dung duyt while(currentlist1)
+                }// dau if
+        //2: khac ten hoac key
+                else
+                {
+                    currentlist1=currentlist1->next;
+                }
+
+                    }// dau while(currentlist1)
+
+                if(mark == 0)
+                    {
+                        newlist->add_Photo(currentlist2->dpi,currentlist2->keyword1,currentlist2->keyword2,currentlist2->keyword3,currentlist2->name);
+
+                    }
+                else
+                   {
+                        newlist->modify(currentlist2->dpi,currentlist2->keyword1,currentlist2->keyword2,currentlist2->keyword3,currentlist2->name);// change dpi
+
+                   }
+                    currentlist2=currentlist2->next;
+                }//dau  while(currentlist2)
+
+
+            }
+
+return *newlist;
+
+    }
+     Lensman operator -(const Lensman &x)  // c=a-b ,  b=x  => a:list 1(con tro this), b:list2s
+    {
+            Lensman *newlist=new Lensman(*this); //copy constructor;
+            Photo *currentlist1=this->head;
+            Photo *currentlist2=x.head;
+
+
+            int a,b,c,d,b1,b2,b3,c1,c2,c3,d1,d2,d3;
+
+                // xet list 2
+                while(currentlist2)
+                {
+
+                    while(currentlist1)
+                    {
+
+                         //CHECK NAME VA KEYWORD
+            a=strcmp(currentlist1->name,currentlist2->name);
+
+            b1=strcmp(currentlist1->keyword1,currentlist2->keyword1);
+            b2=strcmp(currentlist1->keyword1,currentlist2->keyword2);
+            b3=strcmp(currentlist1->keyword1,currentlist2->keyword3);
+
+            c1=strcmp(currentlist1->keyword2,currentlist2->keyword1);
+            c2=strcmp(currentlist1->keyword2,currentlist2->keyword2);
+            c3=strcmp(currentlist1->keyword2,currentlist2->keyword3);
+
+            d1=strcmp(currentlist1->keyword3,currentlist2->keyword1);
+            d2=strcmp(currentlist1->keyword3,currentlist2->keyword2);
+            d3=strcmp(currentlist1->keyword3,currentlist2->keyword3);
+
+        if((b1 == 0) || (b2 == 0) || (b3 == 0))
+            b=0;
+        else
+            b=-1;
+
+
+        if((c1 == 0) || (c2 == 0) || (c3 == 0))
+            c=0;
+        else
+            c=-1;
+
+
+        if((d1 == 0) || (d2 == 0) || (d3 == 0))
+            d=0;
+        else
+            d=-1;
+
+        // cung ten va key
+            if( (a == 0) && ( b==0)&& (c==0) && (d==0) )
+            {
+
+                newlist->remove_Photo(currentlist1->code);
+                break; // ngung duyet
+            }
+            else
+                currentlist1=currentlist1->next; // duyet tiep neu khong cung ten va keyword
+
+                } // dau while(currentlist1)
+                currentlist2= currentlist2->next;
+            }// dau while(currenlist2)
+
+        return *newlist;
+
+    }
+ //   friend Lensman& operator-(const Lensman&, const Lensman&);
+    Lensman()
+    {
+        head=NULL;
+    }
+
+    ~Lensman()
+    {
+        Photo *current=head;
+        while(current)
+        {
+            current=current->next;
+            delete head;
+            head=current;
+        }
+    }
+};//dau ket thuc Class
+
+int main()
+{
+/*
+    Lensman *p1=new Lensman;
+    Lensman *p2=new Lensman;
+    Lensman *p3=new Lensman;
+    Lensman *p4=new Lensman;
+    /////////////////List 1////////////////////////////////////////////
+
+    cout<<"list 1"<<endl<<endl;
+    p1->add_Photo(100,(char *) "slow",(char *) "animal",(char *) "move",(char *) "Horse riding");
+    p1->add_Photo(100,(char *) "slo",(char *) "animal",(char *) "move",(char *) "Horse riding");
+    p1->add_Photo(100,(char *) "slo",(char *) "anima",(char *) "move",(char *) "Horse riding");
+    p1->print();
+    ///////////////////////////////////////////////////////////////////
+
+    /////////////////List 2////////////////////////////////////////////
+
+    cout<<"list 2"<<endl<<endl;
+    p2->add_Photo(100,(char *) "slow",(char *) "animal",(char *) "move",(char *) "Horse riding");
+    p2->add_Photo(100,(char *) "slo",(char *) "animal",(char *) "move",(char *) "Horse riding");
+    p2->add_Photo(100,(char *) "fast",(char *) "anima",(char *) "move",(char *) "Horse riding");
+    p2->print();
+*/
+    ///////////////////////////////////////////////////////////////////
+////////////////////operator + test/////////////
+ /*
+    *p3=(*p1)+(*p2);
+    p3->print();
+*/
+/////////////////////////////////////////////
+
+///////////////////operator- test////////////////
+/*
+    *p4=(*p1)-(*p2);
+    p4->print();
+
+*/
+///////////////////////////////////////////////
+
+
+Lensman p1;
+Lensman p2;
+Lensman p3;
+Lensman p4;
+ /////////////////List 1////////////////////////////////////////////
+
+    cout<<"list 1"<<endl<<endl;
+    p1.add_Photo(100,(char *) "slow",(char *) "animal",(char *) "move",(char *) "Horse riding");
+    p1.add_Photo(100,(char *) "slo",(char *) "animal",(char *) "move",(char *) "Horse riding");
+    p1.add_Photo(100,(char *) "slo",(char *) "anima",(char *) "move",(char *) "Horse riding");
+    p1.print();
+    ///////////////////////////////////////////////////////////////////
+
+    /////////////////List 2////////////////////////////////////////////
+
+    cout<<"list 2"<<endl<<endl;
+    p2.add_Photo(100,(char *) "slow",(char *) "animal",(char *) "move",(char *) "Horse riding");
+    p2.add_Photo(100,(char *) "slo",(char *) "animal",(char *) "move",(char *) "Horse riding");
+    p2.add_Photo(100,(char *) "fast",(char *) "anima",(char *) "move",(char *) "Horse riding");
+    p2.print();
+
+    ///////////////////////////////////////////////////////////////////
+////////////////////operator + test/////////////
+
+    p3=p1+p2;
+    p3.print();
+
+/////////////////////////////////////////////
+
+///////////////////operator- test////////////////
+
+    p4=p1-p2;
+    p4.print();
+
+
+///////////////////////////////////////////////
+    return 0;
+}
+
